e1b0ed74b0615d6e809cec736114c4b9806ab67c
belgoros <s.cambour@gmail.com>
2024-11-04 17:07:31 +0100

    Implement incidents list display

--- lib/heads_up_web/router.ex
+++ lib/heads_up_web/router.ex
@@ -21,6 +21,7 @@ defmodule HeadsUpWeb.Router do
     get "/tips", TipController, :index
     get "/tips/:id", TipController, :show
     live "/effort", EffortLive
+    live "/incidents", IncidentLive.Index
   end
 
   # Other scopes may use custom stacks.
