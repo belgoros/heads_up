f613e6d5bb78446503dea9f34798630c2b0e3bf1
belgoros <s.cambour@gmail.com>
2024-11-18 18:03:24 +0100

    Use query macros in urgent_incidents/1, filter_incidents/0

--- lib/heads_up/incidents.ex
+++ lib/heads_up/incidents.ex
@@ -2,6 +2,7 @@ defmodule HeadsUp.Incidents do
   alias HeadsUp.Incidents.Incident
 
   alias HeadsUp.Repo
+  import Ecto.Query
 
   def list_incidents do
     Repo.all(Incident)
@@ -12,6 +13,19 @@ defmodule HeadsUp.Incidents do
   end
 
   def urgent_incidents(incident) do
-    list_incidents() |> List.delete(incident)
+    Incident
+    |> where([i], i.id != ^incident.id)
+    |> where(status: :pending)
+    |> order_by(:priority)
+    |> limit(3)
+    |> Repo.all()
+  end
+
+  def filter_incidents do
+    Incident
+    |> where(status: :resolved)
+    |> where([i], ilike(i.name, "%in%"))
+    |> order_by(desc: :name)
+    |> Repo.all()
   end
 end
