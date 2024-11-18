26888c1a4e75baab5c87bd16a9deff9bd60fa41d
belgoros <s.cambour@gmail.com>
2024-11-18 18:03:46 +0100

    Use streams and filter_incidents/0

--- lib/heads_up_web/live/incident_live/index.ex
+++ lib/heads_up_web/live/incident_live/index.ex
@@ -5,12 +5,12 @@ defmodule HeadsUpWeb.IncidentLive.Index do
   import HeadsUpWeb.Components.CustomComponents
 
   def mount(_params, _session, socket) do
-    incidents = Incidents.list_incidents()
+    incidents = Incidents.filter_incidents()
 
     socket =
-      assign(socket,
-        incidents: incidents
-      )
+      socket
+      |> assign(:page_title, "Incidents")
+      |> stream(:incidents, incidents)
 
     {:ok, socket}
   end
@@ -24,18 +24,23 @@ defmodule HeadsUpWeb.IncidentLive.Index do
           Thanks for pitching in. <%= vibe %>
         </:tagline>
       </.headline>
-      <div class="incidents">
-        <.incident_card :for={incident <- @incidents} incident={incident} />
+      <div class="incidents" id="incidents" phx-update="stream">
+        <.incident_card
+          :for={{dom_id, incident} <- @streams.incidents}
+          incident={incident}
+          id={dom_id}
+        />
       </div>
     </div>
     """
   end
 
   attr :incident, HeadsUp.Incidents.Incident, required: true
+  attr :id, :string, required: true
 
   def incident_card(assigns) do
     ~H"""
-    <.link navigate={~p"/incidents/#{@incident}"}>
+    <.link navigate={~p"/incidents/#{@incident}"} id={@id}>
       <div class="card">
         <img src={@incident.image_path} />
         <h2><%= @incident.name %></h2>
