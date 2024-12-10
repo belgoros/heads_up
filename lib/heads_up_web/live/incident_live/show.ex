e34c9d4e8359e52f00b817a8fc61b4eebc878530
belgoros <s.cambour@gmail.com>
2024-12-10 15:57:01 +0100

    Implement asynchronous assignment

--- lib/heads_up_web/live/incident_live/show.ex
+++ lib/heads_up_web/live/incident_live/show.ex
@@ -16,7 +16,10 @@ defmodule HeadsUpWeb.IncidentLive.Show do
     socket =
       socket
       |> assign(:incident, incident)
-      |> assign(:urgent_incidents, Incidents.urgent_incidents(incident))
+      |> assign(:page_title, incident.name)
+      |> assign_async(:urgent_incidents, fn ->
+        {:ok, %{urgent_incidents: Incidents.urgent_incidents(incident)}}
+      end)
 
     {:noreply, socket}
   end
@@ -55,13 +58,25 @@ defmodule HeadsUpWeb.IncidentLive.Show do
     ~H"""
     <section>
       <h4>Urgent Incidents</h4>
-      <ul class="incidents">
-        <li :for={incident <- @incidents}>
-          <.link navigate={~p"/incidents/#{incident}"}>
-            <img src={incident.image_path} /> <%= incident.name %>
-          </.link>
-        </li>
-      </ul>
+      <.async_result :let={result} assign={@incidents}>
+        <:loading>
+          <div class="loading">
+            <div class="spinner"></div>
+          </div>
+        </:loading>
+        <:failed :let={{:error, reason}}>
+          <div class="failed">
+            Whoops: <%= reason %>
+          </div>
+        </:failed>
+        <ul class="incidents">
+          <li :for={incident <- result}>
+            <.link navigate={~p"/incidents/#{incident}"}>
+              <img src={incident.image_path} /> <%= incident.name %>
+            </.link>
+          </li>
+        </ul>
+      </.async_result>
     </section>
     """
   end
