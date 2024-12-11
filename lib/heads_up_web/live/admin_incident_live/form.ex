cfb3e8a105329eb0db6ab6e89d625cf79dc53d72
belgoros <s.cambour@gmail.com>
2024-12-11 12:45:10 +0100

    Implement new incident form

---
+++ lib/heads_up_web/live/admin_incident_live/form.ex
@@ -0,0 +1,42 @@
+defmodule HeadsUpWeb.AdminIncidentLive.Form do
+  use HeadsUpWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    socket =
+      socket
+      |> assign(:page_title, "New Incident")
+      |> assign(:form, to_form(%{}, as: "incident"))
+
+    {:ok, socket}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <.header>
+      {@page_title}
+    </.header>
+    <.simple_form for={@form} id="incident-form">
+      <.input field={@form[:name]} label="Name" />
+      <.input field={@form[:priority]} label="Priority" />
+      <.input
+        field={@form[:status]}
+        type="select"
+        label="Status"
+        prompt="Choose a status"
+        options={[:pending, :resolved, :canceled]}
+      />
+
+      <.input field={@form[:description]} type="textarea" label="Description" />
+      <.input field={@form[:image_path]} label="Image path" />
+      <:actions>
+        <.button>
+          Save Incident
+        </.button>
+      </:actions>
+    </.simple_form>
+    <.back navigate={~p"/admin/incidents"}>
+      Back
+    </.back>
+    """
+  end
+end
