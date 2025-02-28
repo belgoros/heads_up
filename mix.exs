213ce9ca33efeb7938d84907e1a7636e23ef274b
belgoros <s.cambour@gmail.com>
2025-02-28 11:47:11 +0100

    Use phx.gen.auth generator to create auth system

--- mix.exs
+++ mix.exs
@@ -32,6 +32,7 @@ defmodule HeadsUp.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:bcrypt_elixir, "~> 3.0"},
       {:phoenix, "~> 1.7.14"},
       # {:phoenix, github: "phoenixframework/phoenix", override: true},
       {:phoenix_ecto, "~> 4.5"},
