625fe2cc007ff5a47129e66530325ed768a21c24
belgoros <s.cambour@gmail.com>
2025-03-12 10:25:09 +0100

    Add association relations to User and Incident schema

--- lib/heads_up/accounts/user.ex
+++ lib/heads_up/accounts/user.ex
@@ -11,6 +11,7 @@ defmodule HeadsUp.Accounts.User do
     field :hashed_password, :string, redact: true
     field :current_password, :string, virtual: true, redact: true
     field :confirmed_at, :utc_datetime
+    has_many :responses, HeadsUp.Responses.Response
 
     timestamps(type: :utc_datetime)
   end
